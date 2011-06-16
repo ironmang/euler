0d31822c709fe4a4ec3aa09739db04aaf7d900a4
Mike L <ironmang@gmail.com>
2011-06-15 22:36:03 -0400

    problem1 solved!

---
+++ problem1.py
@@ -0,0 +1,15 @@
+def get_natural_sum():
+    sumof_multiples = 0
+    
+    for i in range(0, 1000):
+        if i % 15 == 0:
+            sumof_multiples += i
+        elif i% 5 == 0:
+            sumof_multiples += i
+        elif i % 3 == 0:
+            sumof_multiples += i
+
+    return sumof_multiples
+
+natural_sum = get_natural_sum()
+print "NATURAL SUM: %d" % natural_sum
